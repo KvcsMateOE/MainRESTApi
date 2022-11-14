58711f1a26327c73963b7906bbae4e9a68e89921
Mate0714LH@stud.uni-obuda.hu <kovacsm0072@gmail.com>
2022-11-14 19:40:23 +0100

    Book.cs has been added

---
+++ WCZ6Y1_HFT_2022231.Models/Book.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCZ6Y1_HFT_2022231.Models
+{
+    public class Book
+    {
+        public Book()
+        {
+            Authors = new HashSet<Author>();
+            Publishers = new HashSet<Publisher>();
+            Title = string.Empty;
+            Translator = string.Empty;
+        }
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int BookId { get; set; }
+
+        [Required]
+        [MaxLength(64)]
+        public string Title { get; set; }
+
+        [Required]
+        public int ReleaseYear { get; set; }
+
+        public string? Genre { get; set; }
+
+        [Required]
+        public string Translator { get; set; }
+
+        //public int? Budget { get; set; }
+
+        public double? Rating { get; set; }
+
+        public virtual ICollection<Author> Authors { get; }
+        public virtual ICollection<Publisher> Publishers { get; }
+
+        public override string ToString()
+        {
+            return $"MovieId: {BookId}\nMovie name: {Title}\nRelease year: {ReleaseYear}\nGenre: {Genre}\nDirector: {Translator}\nRating: {Rating}\n";
+        }
+    }
+}
