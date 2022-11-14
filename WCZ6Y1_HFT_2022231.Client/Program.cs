48590974dc759381a0bc32826ae9fcf0fb474fb3
Mate0714LH@stud.uni-obuda.hu <kovacsm0072@gmail.com>
2022-11-14 19:48:22 +0100

    Client project completed

--- WCZ6Y1_HFT_2022231.Client/Program.cs
+++ WCZ6Y1_HFT_2022231.Client/Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
+using WCZ6Y1_HFT_2022231.Client.Menu;
 
 namespace WCZ6Y1_HFT_2022231.Client
 {
     class Program
     {
+        //35742 //http://localhost:35742"
+        public static RestService REST_API = new RestService("http://localhost:35742");
         static void Main(string[] args)
         {
-            
+            try
+            {
+                MenuSystem.ConfigureMenu(args);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"{e.Message}\nThe application will close now.");
+
+            }
         }
     }
 }
