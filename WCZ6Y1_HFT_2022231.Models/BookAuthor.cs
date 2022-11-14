e9a25dc947d6ef1d1ace5ae8ee4a588bffd9632b
Mate0714LH@stud.uni-obuda.hu <kovacsm0072@gmail.com>
2022-11-14 19:41:45 +0100

    Models project completed

---
+++ WCZ6Y1_HFT_2022231.Models/BookAuthor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCZ6Y1_HFT_2022231.Models
+{
+   public class BookAuthor
+    {
+        public int BookId { get; set; }
+        public Book Book { get; set; }
+
+        public int AuthorId { get; set; }
+        public Author Author { get; set; }
+    }
+}
