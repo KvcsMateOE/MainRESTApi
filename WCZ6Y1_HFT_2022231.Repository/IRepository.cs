8fdae8babc727f3b3eb37595b388c70da92b1b55
Mate0714LH@stud.uni-obuda.hu <kovacsm0072@gmail.com>
2022-11-14 19:49:39 +0100

    IRepositroy interface created

---
+++ WCZ6Y1_HFT_2022231.Repository/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCZ6Y1_HFT_2022231.Repository
+{
+    public interface IRepository<T> where T : class
+    {
+        void Create(T entity);
+        IQueryable<T> ReadAll();
+        T ReadOne(int Id);
+        void Update(T oldEntity, T newEntity);
+        void Remove(T entity);
+    }
+}
