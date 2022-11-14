8110643bbf443f886ac446785abb7f2df61e7c78
Mate0714LH@stud.uni-obuda.hu <kovacsm0072@gmail.com>
2022-11-14 19:53:32 +0100

    BookRepo is done

---
+++ WCZ6Y1_HFT_2022231.Repository/BookRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WCZ6Y1_HFT_2022231.Models;
+
+namespace WCZ6Y1_HFT_2022231.Repository
+{
+    public class BookRepository : Repository<Book>
+    {
+        public BookRepository(PublisherDbContext publisherDb) : base(publisherDb)
+        {
+        }
+
+        public override Book ReadOne(int Id)
+        {
+            return ReadAll().Where(x => x.BookId.Equals(Id)).FirstOrDefault();
+        }
+
+        public override void Update(Book oldEntity, Book newEntity)
+        {
+            if (oldEntity != null && newEntity != null)
+            {
+                oldEntity.ReleaseYear = newEntity.ReleaseYear;
+                oldEntity.Rating = newEntity.Rating;
+                oldEntity.Genre = newEntity.Genre;
+                oldEntity.Translator = newEntity.Translator;
+                oldEntity.Title = newEntity.Title;
+                PublisherDb.SaveChanges();
+            }
+        }
+    }
+}
