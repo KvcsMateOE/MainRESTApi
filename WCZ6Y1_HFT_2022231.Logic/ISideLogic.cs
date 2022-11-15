ca7d8ac726da2a74bdaf14d689b37176810ced3a
Mate0714LH@stud.uni-obuda.hu <kovacsm0072@gmail.com>
2022-11-15 12:04:11 +0100

    ISideLogic interface created

---
+++ WCZ6Y1_HFT_2022231.Logic/ISideLogic.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCZ6Y1_HFT_2022231.Logic
+{
+    public interface ISideLogic
+    {
+        IEnumerable<string> ListPublisherByPrintingCapacity();
+        IEnumerable<string> GetAllAuthorsByCountry(string country);
+        IEnumerable<int> GetAllOldBooks(int yearOfBoomers);
+        IEnumerable<ComparedBooks> HackIMDB(int yearOfBoomers);
+        IEnumerable<string> GetLastCheapestPublisher();
+        IEnumerable<string> GetAllActionBooksWithMoreRatingThan2();
+    }
+}
+
