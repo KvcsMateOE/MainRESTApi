ca14f778f54833d7da09e7975c411c486c424d4e
Mate0714LH@stud.uni-obuda.hu <kovacsm0072@gmail.com>
2022-11-15 23:56:02 +0100

    AuthorController.cs created

---
+++ WCZ6Y1_HFT_2022231.Endpoint/Controllers/AuthorController.cs
@@ -0,0 +1,53 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WCZ6Y1_HFT_2022231.Logic;
+using WCZ6Y1_HFT_2022231.Models;
+
+namespace WCZ6Y1_HFT_2022231.Endpoint.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AuthorController : ControllerBase
+    {
+        IMainLogic logic;
+
+        public AuthorController(IMainLogic logic)
+        {
+            this.logic = logic;
+        }
+
+        [HttpGet]
+        public IEnumerable<Author> ReadAll()
+        {
+            return logic.ReadAllAuthor();
+        }
+
+        [HttpGet("{id}")]
+        public Author Read(int id)
+        {
+            return logic.ReadAuthor(id);
+        }
+
+        [HttpPost]
+        public void Create([FromBody] Author entity)
+        {
+            logic.CreateAuthor(entity);
+        }
+
+        [HttpPut("{id}")]
+        public void Update(int id, [FromBody] Author entity)
+        {
+            logic.UpdateAuthor(id, entity);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            logic.DeleteAuthor(id);
+        }
+    }
+}
